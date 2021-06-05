23ed042bb5389229c5e8ca05e84f69b19317682b
s18324 <s18324@pjwstk.edu.pl>
2021-06-05 14:30:05 +0200

    Added thymeleaf and controller for displaying notes
    The second controller has been added due to thymeleaf incompatibility with the REST controller

--- src/main/java/com/example/notesbackend/NoteController.java
+++ src/main/java/com/example/notesbackend/NoteController.java
@@ -1,48 +1,24 @@
 package com.example.notesbackend;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 @RequestMapping(path = "notes")
 public class NoteController {
 
     private final NoteService noteService;
 
-    @Autowired
     public NoteController(NoteService noteService) {
         this.noteService = noteService;
     }
 
     @GetMapping
-    public List<NoteDTO> getNotes() {
-        return noteService.getNotes();
-    }
-
-    @GetMapping("/{id}")
-    public NoteDTO getNoteById(@PathVariable Long id) {
-        return noteService.getNote(id);
-    }
-
-    @PostMapping("/add")
-    public void createNewNote(@RequestBody NoteCreateRequestDTO noteCreateRequestDTO) {
-        noteService.createNewNote(noteCreateRequestDTO);
-    }
-
-    @PutMapping("/{id}")
-    public void updateNote(@PathVariable Long id, @RequestBody NoteCreateRequestDTO noteCreateRequestDTO) {
-        noteService.updateNote(id, noteCreateRequestDTO);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteNote(@PathVariable Long id) {
-        noteService.deleteNote(id);
-    }
-
-    @GetMapping("/history/{id}")
-    public List<Modification> getNoteHistory(@PathVariable Long id) {
-        return noteService.getNoteHistory(id);
+    public String getNotes(Model model) {
+        model.addAttribute("notes", noteService.getNotes());
+        return "notes";
     }
 
 }
---
+++ src/main/java/com/example/notesbackend/NoteRestController.java
@@ -0,0 +1,43 @@
+package com.example.notesbackend;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "notes")
+public class NoteRestController {
+
+    private final NoteService noteService;
+
+    @Autowired
+    public NoteRestController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @GetMapping("/{id}")
+    public NoteDTO getNoteById(@PathVariable Long id) {
+        return noteService.getNote(id);
+    }
+
+    @PostMapping("/add")
+    public void createNewNote(@RequestBody NoteCreateRequestDTO noteCreateRequestDTO) {
+        noteService.createNewNote(noteCreateRequestDTO);
+    }
+
+    @PutMapping("/{id}")
+    public void updateNote(@PathVariable Long id, @RequestBody NoteCreateRequestDTO noteCreateRequestDTO) {
+        noteService.updateNote(id, noteCreateRequestDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteNote(@PathVariable Long id) {
+        noteService.deleteNote(id);
+    }
+
+    @GetMapping("/history/{id}")
+    public List<Modification> getNoteHistory(@PathVariable Long id) {
+        return noteService.getNoteHistory(id);
+    }
+
+}
