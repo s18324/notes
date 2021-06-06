0fafd1469b47285259cc4c4f1796c78a148bdaba
s18324 <s18324@pjwstk.edu.pl>
2021-06-06 15:55:34 +0200

    Added popup for displaying notes content

--- src/test/java/com/example/notesbackend/NoteServiceTest.java
+++ src/test/java/com/example/notesbackend/NoteServiceTest.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,11 +18,11 @@ import static org.junit.Assert.*;
 @SpringBootTest
 public class NoteServiceTest {
 
-    private final ModelMapper modelMapper = new ModelMapper();
     @Autowired
     private NoteRepository noteRepository;
     @Autowired
     private ModificationRepository modificationRepository;
+
     private NoteService noteService;
     private Note note;
 
