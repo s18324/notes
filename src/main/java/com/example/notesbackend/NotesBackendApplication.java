eef2ea0b4318448568b0fad94f8ef35afc1eee64
s18324 <s18324@pjwstk.edu.pl>
2021-06-07 14:04:00 +0200

    Removed unused logger

--- src/main/java/com/example/notesbackend/NotesBackendApplication.java
+++ src/main/java/com/example/notesbackend/NotesBackendApplication.java
@@ -1,14 +1,10 @@
 package com.example.notesbackend;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotesBackendApplication {
-
-    private static final Logger log = LoggerFactory.getLogger(NotesBackendApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(NotesBackendApplication.class, args);
